04de3f0e0ba6ff0f6e06b0a97ee4b8cbf57d27ab
meta0rc <70641868+meta0rc@users.noreply.github.com>
2022-06-02 17:21:10 -0300

    Update Topics.tsx

--- src/components/topics/Topics.tsx
+++ src/components/topics/Topics.tsx
@@ -1,5 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 import './topics.css'
+import Serralheiro from "../../assets/images/statics/serralheria.jpg"
+import Arquiteto from "../../assets/images/statics/arquiteto.jpg"
+import Programacao from "../../assets/images/statics/programacao.jpg"
+
 export const Topics = () => {
     const navigate = useNavigate()
     return (
@@ -7,7 +11,7 @@ export const Topics = () => {
                 
             <ul className="topics">
                 <li className="item">
-                    <img src="src/assets/images/statics/serralheria.jpg" alt="Serralheiria" width={600} height={400} />
+                    <img src={Serralheiro} alt="Serralheiria" width={600} height={400} />
                     <div className="collum-flex">
                         <h2>Encontre Serralheiros perto de Você</h2>
                         <button className="btn-info" onClick={() => navigate('/findJob/Serralheiro')}>
@@ -17,17 +21,17 @@ export const Topics = () => {
 
                 </li>
                 <li className="item">
-                <img src="src/assets/images/statics/arquiteto.jpg" alt="Arquitetura" width={600} height={400} className="mobile-image"/>
+                <img src={Arquiteto} alt="Arquitetura" width={600} height={400} className="mobile-image"/>
                 <div className="collum-flex">
                     <h2>Encontre Arquitetos perto de Você</h2>
                     <button className="btn-info"  onClick={() => navigate('/findJob/arquiteto')}>
                         Saiba Mais
                     </button>
                 </div>
-                    <img src="src/assets/images/statics/arquiteto.jpg" alt="Arquitetura" width={600} height={400} className="desktop-image" />
+                    <img src={Arquiteto} alt="Arquitetura" width={600} height={400} className="desktop-image" />
                 </li>
                 <li className="item">
-                    <img src="src/assets/images/statics/programacao.jpg" alt="Programacao" width={600} height={400} />
+                    <img src={Programacao} alt="Programacao" width={600} height={400} />
                 <div className="collum-flex">
                     <h2>Encontre Profissionais de Programação</h2>
                     <button className="btn-info">
